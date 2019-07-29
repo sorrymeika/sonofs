739d6214500a223a7d94263a8cad862468bbfee6
孙路 <>
2019-07-29 18:07:52 +0800

    add client

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 const { createMaster, createServer } = require('./lib/server');
+const { createClient } = require('./lib/client');
+const { startRegistry } = require('./lib/registry');
 
-exports.startUploadMaster = createMaster;
-exports.startUploadServer = createServer;
+exports.startServerMaster = createMaster;
+exports.startServer = createServer;
+exports.createClient = createClient;
+exports.startRegistry = startRegistry;
